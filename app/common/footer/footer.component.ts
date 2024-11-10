bd5fd57b1a9d85bcebe348e43129b463cd858e32
jlakvidu <jlakvidu@gmail.com>
2024-11-10 21:54:55 +0530

    third commit

--- app/common/footer/footer.component.ts
+++ app/common/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Renderer2, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
@@ -12,19 +12,50 @@ import { filter } from 'rxjs/operators';
 })
 export class FooterComponent implements OnInit {
   showFooter = true;
+  isMobileKeyboardOpen = false;
+  private excludedRoutes = ['/login', '/donor-form', '/signin', '/donor-registration'];
   
-  constructor(private router: Router) {
-    // Subscribe to router events to check current route
+  constructor(
+    private router: Router,
+    private renderer: Renderer2,
+    private el: ElementRef
+  ) {
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
-    ).subscribe((event: any) => {
-      // Hide footer on login and signup pages
-      this.showFooter = !['/login', '/hospital-sign-up'].includes(event.url);
+    ).subscribe(() => {
+      this.checkRoute();
     });
   }
 
   ngOnInit() {
-    // Check initial route
-    this.showFooter = !['/login', '/hospital-sign-up'].includes(this.router.url);
+    this.checkRoute();
+    
+    if (this.isMobileDevice()) {
+      this.onResize(null);
+    }
+  }
+
+  private checkRoute() {
+    const currentUrl = this.router.url;
+    this.showFooter = !this.excludedRoutes.some(route => currentUrl.includes(route));
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    const windowHeight = window.innerHeight;
+    const screenHeight = window.screen.height;
+    this.isMobileKeyboardOpen = windowHeight < screenHeight * 0.75;
+    
+    if (this.isMobileKeyboardOpen) {
+      this.renderer.setStyle(this.el.nativeElement.querySelector('.footer'), 'display', 'none');
+    } else {
+      if (this.showFooter) {
+        this.renderer.setStyle(this.el.nativeElement.querySelector('.footer'), 'display', 'block');
+      }
+    }
+  }
+
+  isMobileDevice(): boolean {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
   }
 }
